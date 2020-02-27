d5e2ec14cffe2676cad66c71232e4afc0e5792a3
hafezip6020 <33041123+hafezip6020@users.noreply.github.com>
2020-02-27 00:13:32 -0800

    Update TriangleFive.java

--- APCSA 2020 V2/src/unit7/TriangleFive.java
+++ APCSA 2020 V2/src/unit7/TriangleFive.java
@@ -16,19 +16,43 @@ public class TriangleFive
 
 	public TriangleFive(char c, int amt)
 	{
+		letter = c;
+		amount = amt;
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
 		String output="";
+		
+		for (int i = amount; i > 0; i--) {
+			for (int x = 0; x<i ; x++) {
+				output = output + letter;
+			}
+			output += " ";
+			if (letter == 'Z')
+				letter = 'A';
+			else
+				letter = (char) (letter + 1);
+		}
+			String output2 = output;
+			int size = 0;
+			int value = 2, add = 3;
+			for (int i = amount; i > 0; i--) {
+				output = output + "\n" + output2.substring(0, output2.length() - value);
+				value = value + add;
+				add =  add + 1;
+			}
+		
 		return output;
 	}
 }
