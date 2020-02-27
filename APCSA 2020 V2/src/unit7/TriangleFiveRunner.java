594ab1fa0ea60cf61a83ce36e1d48624c753b924
hafezip6020 <33041123+hafezip6020@users.noreply.github.com>
2020-02-27 00:14:03 -0800

    Update TriangleFiveRunner.java

--- APCSA 2020 V2/src/unit7/TriangleFiveRunner.java
+++ APCSA 2020 V2/src/unit7/TriangleFiveRunner.java
@@ -9,5 +9,19 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
+	   TriangleFive T = new TriangleFive('C',4);
+	   System.out.println(T);
+	   T.setLetter('A');
+	   T.setAmount(5);
+	   System.out.println(T.toString());
+	   T.setLetter('B');
+	   T.setAmount(7);
+	   System.out.println(T.toString());
+	   T.setLetter('X');
+	   T.setAmount(6);
+	   System.out.println(T.toString());
+	   T.setLetter('Z');
+	   T.setAmount(8);
+	   System.out.println(T.toString());
 	}
 }
