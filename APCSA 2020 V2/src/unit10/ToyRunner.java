baefb5c4eaf51f55a91d9d5dab22d6e36b806adb
hafezip6020 <hafezip6020@CA-SU-F106-14.sduhsd.lan>
2020-03-06 10:49:01 -0800

    gr

---
+++ APCSA 2020 V2/src/unit10/ToyRunner.java
@@ -0,0 +1,20 @@
+package unit10;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class ToyRunner
+{
+	public static void main(String[] args)
+	{
+		Toy t = new Toy();
+		t.setName("sorry");
+		t.setCount(1);
+		System.out.println(t.toString());
+		t.setName("gi joe");
+		t.setCount(5);
+		System.out.println(t.toString());
+	}
+}
