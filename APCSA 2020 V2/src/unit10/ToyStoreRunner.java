003f6185164bea2af00fd233c1636da5c0c9b369
hafezip6020 <33041123+hafezip6020@users.noreply.github.com>
2020-03-08 14:30:08 -0700

    Update ToyStoreRunner.java

--- APCSA 2020 V2/src/unit10/ToyStoreRunner.java
+++ APCSA 2020 V2/src/unit10/ToyStoreRunner.java
@@ -12,6 +12,13 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
-
+		ToyStore1 ts = new ToyStore1("sorry bat sorry sorry sorry train train teddy teddy ball ball");
+		System.out.println(ts);
+		
+		ToyStore1 sto = new ToyStore1();
+		System.out.println( sto );
+		sto.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball" );
+		System.out.println( sto );	
+		System.out.println( "max == " + sto.getMostFrequentToy() );	
 	}
 }
